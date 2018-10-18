d77cdcd4b094999b06151f2de7b6270097c78834
Willie Cadete <williecadete@gmail.com>
2018-10-18 20:05:02 -0300

    Add JS and fixes

--- js/libs/main.js
+++
@@ -1,5 +0,0 @@
-$(".rslides").responsiveSlides({
-  auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
-});
